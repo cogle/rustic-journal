8b8942f699ef2946fc077b2e4ccaccb2d9baf06c
cogle <thecogle@gmail.com>
2020-08-23 17:54:21 -0700

    Continue working on getting the very basics to work

--- src/core/journal/journal.rs
+++ src/core/journal/journal.rs
@@ -1,5 +1,20 @@
 use crate::sys;
 
-pub struct Journal {
-    journal: mut* sys::sd_journal;
+#[derive(Debug)]
+pub struct CJournalError {
+    pub message: String,
+    pub error_code: c_int
 };
+
+pub struct Journal {
+    journal_handle: mut* sys::sd_journal;
+};
+
+impl Journal {
+    pub fn new() -> Result<Journal, CJournalError> {
+        CJournalError{
+            message: "Test",
+            error_code: -1
+        };
+    }
+}
