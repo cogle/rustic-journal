09396e91cfc38f612389e0edb9d9947672b47e9c
cogle <thecogle@gmail.com>
2020-08-24 19:27:47 -0700

    Formatted

--- src/core/journal/journal.rs
+++ src/core/journal/journal.rs
@@ -5,18 +5,19 @@ use std::os::raw::c_int;
 #[derive(Debug)]
 pub struct CJournalError {
     pub message: String,
-    pub error_code: c_int
+    pub error_code: c_int,
 }
 
 pub struct Journal {
-    journal_handle: *mut sys::sd_journal
+    journal_handle: *mut sys::sd_journal,
 }
 
 impl Journal {
-    pub fn new() /*-> Result<Journal, CJournalError>*/ {
+    pub fn new() /*-> Result<Journal, CJournalError>*/
+    {
         /*
         let mut handle = std::ptr::null_mut() as *sys::sd_journal;
-        
+
         let rc =  unsafe  {
             sys::sd_journal_open(&mut handle, sys::SD_JOURNAL_LOCAL_ONLY);
         }
