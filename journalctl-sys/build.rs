8847b41200460efa3b8790679d6beb8acd726edf
cogle <thecogle@gmail.com>
2020-08-25 22:16:26 -0700

    Update the build script so it actually links

--- journalctl-sys/build.rs
+++ journalctl-sys/build.rs
@@ -2,4 +2,6 @@ extern crate pkg_config;
 
 fn main() {
     pkg_config::probe_library("systemd").unwrap();
+    // TODO update below to actually get it to compile with tests
+    // println!("cargo:rustc-link-lib=systemd")
 }
